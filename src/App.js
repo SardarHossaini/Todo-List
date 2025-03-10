75dba34f4fbc0d76578813a8906e7aa9914f9d27
Sardar219 <sardarhossaini2022@gmail.com>
2025-03-10 20:17:06 +0430

    added fontawasome icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import Navbar from "./Navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  
   return (
     <div className="app">
-      
+      <i className="fa fa-home"></i>
     </div>
   );
 }
