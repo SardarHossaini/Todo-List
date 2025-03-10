c9accc22bc209cb78d395531425e676dc9e7900f
Sardar219 <sardarhossaini2022@gmail.com>
2025-03-10 21:07:58 +0430

    added form of input task

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 const TodoForm = () => {
   const [value, setValue] = useState("");
-  const handleChange = () => {
-    alert("Yes");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
   };
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="todo-input"
         placeholder="what is the task today?"
-        onChange={handleChange}
+        onChange={(e) => setValue(e.target.value)}
       />
       <button type="submit" className="todo-btn">
         Add Task
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,6 +1,7 @@
 import TodoForm from "./TodoForm";
-
+import { useState } from "react";
 const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="TodoWrapper">
       <TodoForm />
